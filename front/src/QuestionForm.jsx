c81dac23bd9758b670fb0a1948366d1d26602790
佐藤大介 <workshift56@gmail.com>
2018-09-17 12:01:28 +0900

    add fetch to button

--- front/src/QuestionForm.jsx
+++ front/src/QuestionForm.jsx
@@ -4,7 +4,8 @@ export default class QuestionForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      content: ""
+      content: "",
+      questionID: 1
     };
     this.changeText = this.changeText.bind(this);
     this.clickNextButton = this.clickNextButton.bind(this);
@@ -14,15 +15,45 @@ export default class QuestionForm extends React.Component {
     this.setState({ content: e.target.value });
   }
 
-  clickNextButton(){
-    this.setState({ content: "" })
+  clickNextButton() {
+    const answer = {
+      question_id: this.state.question_id,
+      input: this.state.content
+    };
+
+    fetch("/results/" + this.state.questionID, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(answer)
+    })
+      .then(resp => {
+        if (resp.status === 201 || resp.status === 200) {
+          return resp;
+        }
+        throw new Error(resp.statusText);
+      })
+      .then(x => x.json())
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.error("post message error: ", err);
+      });
   }
 
   render() {
     return (
       <div>
-          <div></div>
-        <input type="text" value={this.state.content} name="hello" onChange={this.changeText} />
+        <div>問題を表示します</div>
+        <input
+          type="text"
+          value={this.state.content}
+          name="hello"
+          onChange={this.changeText}
+        />
         <button onClick={this.clickNextButton}>次へ</button>
       </div>
     );
